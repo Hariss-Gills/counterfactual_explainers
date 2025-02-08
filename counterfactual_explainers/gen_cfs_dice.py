9334b4332f6bce985fa9fd613b0d419c8d9f9b9d
Hariss Ali Gills <harissgills@gmail.com>
2025-02-08 18:04:43 +0000

    generate more Dice cfs that failed before.

--- counterfactual_explainers/gen_cfs_dice.py
+++ counterfactual_explainers/gen_cfs_dice.py
@@ -11,13 +11,14 @@ from sklearn.model_selection import train_test_split
 
 from counterfactual_explainers.data.preprocess_data import (
     clean_config,
+    create_data_transformer,
     read_compas_dataset,
     read_dataset,
 )
 
 
-# NOTE: For now genrate using RF model only
-# TODO: measure runtime
+# TODO: Metrics like runtime cannot be measured post-hoc
+# So this will have to be done here
 def main():
     package = files("counterfactual_explainers")
     toml_path = package / "config.toml"
@@ -48,6 +49,76 @@ def main():
 
             all_feat = features.columns.values.tolist()
             act_feat = list(set(all_feat) - set(non_act_features))
+
+            model = None
+            func = None
+            backend = None
+            method = None
+
+            if model_name == "RF":
+                model = load_scikit_model(
+                    "counterfactual_explainers/models/"
+                    f"{model_name}_{dataset}.pkl"
+                )
+
+                backend = "sklearn"
+                method = "genetic"
+                func = None
+
+            if model_name == "DNN":
+                preprocessor, target_encoder = create_data_transformer(
+                    continuous_features=continuous_features,
+                    categorical_features=categorical_features,
+                )
+                preprocessor.fit(features)
+                print(preprocessor)
+                cont_imputer = preprocessor.named_transformers_[
+                    "continuous"
+                ].named_steps["imputer"]
+                cat_imputer = preprocessor.named_transformers_[
+                    "categorical"
+                ].named_steps["imputer"]
+
+                new_feat_cont = cont_imputer.fit_transform(
+                    features[continuous_features]
+                )
+
+                new_feat_cont = pd.DataFrame(
+                    new_feat_cont,
+                    columns=continuous_features,
+                    index=features.index,
+                )
+
+                # HACK: this is dirty but
+                # does the job
+                if dataset != "fico":
+                    new_feat_cat = cat_imputer.fit_transform(
+                        features[categorical_features]
+                    )
+
+                    new_feat_cat = pd.DataFrame(
+                        new_feat_cat,
+                        columns=categorical_features,
+                        index=features.index,
+                    )
+                    features = pd.concat([new_feat_cont, new_feat_cat], axis=1)
+                else:
+                    features = new_feat_cont
+                transformed_target = target_encoder.fit_transform(target)
+                target = pd.DataFrame(
+                    transformed_target,
+                    columns=[target.name],
+                    index=target.index,
+                )
+
+                model = load_keras_model(
+                    f"counterfactual_explainers/models/{model_name}"
+                    f"_{dataset}.keras"
+                )
+                backend = "TF2"
+                method = "gradient"
+                func = "ohe-min-max"
+
             X_train, X_test, y_train, y_test = train_test_split(
                 features,
                 target,
@@ -55,122 +126,70 @@ def main():
                 random_state=seed,
                 stratify=target,
             )
-            if model_name == "RF":
-                model = load_scikit_model(
-                    f"counterfactual_explainers/models/{model_name}_{dataset}.pkl"
-                )
-                combined_train_df = pd.concat([X_train, y_train], axis=1)
-                dice_data_object = Data(
-                    dataframe=combined_train_df,
-                    continuous_features=continuous_features.tolist(),
-                    outcome_name=params_dataset["target_name"],
-                )
-                dice_model_object = Model(model=model, backend="sklearn")
-                dice_exp = Dice(
-                    dice_data_object, dice_model_object, method="genetic"
-                )
 
-                # WARNING: this instance for fico always fails but
-                # should probably be kept. Also if num_required_cfs = 1
-                # it throws a diffrent error hence catch that here too
-                query_instance = X_test.sample(random_state=seed)
+            combined_train_df = pd.concat([X_train, y_train], axis=1)
+            dice_data_object = Data(
+                dataframe=combined_train_df,
+                continuous_features=continuous_features.tolist(),
+                outcome_name=params_dataset["target_name"],
+            )
+            dice_model_object = Model(model=model, backend=backend, func=func)
+            dice_exp = Dice(dice_data_object, dice_model_object, method=method)
 
-                for num_required_cfs in range(1, 21):
-                    try:
-                        explanation = dice_exp.generate_counterfactuals(
-                            query_instance,
-                            total_CFs=num_required_cfs,
-                            desired_class=desired_class,
-                            features_to_vary=act_feat,
-                        )
-                        cfs_for_all_queries = explanation.cf_examples_list
-                        cfs = cfs_for_all_queries[
-                            0
-                        ]  # Always have one query instance
-                        results_path = Path(
-                            "./counterfactual_explainers/results"
-                        )
-                        results_path.mkdir(parents=True, exist_ok=True)
-                        # df_results = pd.DataFrame(results)
+            # WARNING: this instance for fico RF mostly fails but
+            # should probably be kept. Also if num_required_cfs = 1
+            # it throws a different error, hence catch that here too.
+            # compas DNN always fails too -> no CFs found.
+            query_instance = X_test.sample(random_state=seed)
+            results_path = Path("./counterfactual_explainers/results")
+            results_path.mkdir(parents=True, exist_ok=True)
+            query_instance.to_csv(
+                results_path
+                / f"cf_dice_{model_name}_{dataset}_query_instance.csv",
+                index=False,
+            )
+            print(query_instance)
+            for num_required_cfs in range(1, 21):
+                try:
+                    explanation = dice_exp.generate_counterfactuals(
+                        query_instance,
+                        total_CFs=num_required_cfs,
+                        desired_class=desired_class,
+                        features_to_vary=act_feat,
+                    )
+                    cfs_for_all_queries = explanation.cf_examples_list
+                    cfs = cfs_for_all_queries[
+                        0
+                    ]  # Always have one query instance
+                    results_path = Path("./counterfactual_explainers/results")
+                    results_path.mkdir(parents=True, exist_ok=True)
+                    # df_results = pd.DataFrame(results)
+                    print(cfs.final_cfs_df)
+
+                    # NOTE: need this condtion since it doesn't
+                    # do this check for deep learning methods.
+                    if not cfs.final_cfs_df.empty:
                         cfs.final_cfs_df.to_csv(
-                            results_path
-                            / f"cf_dice_{model_name}_{dataset}_{num_required_cfs}.csv",
+                            results_path / f"cf_dice_{model_name}_{dataset}"
+                            f"_{num_required_cfs}.csv",
                             index=False,
                         )
-                        # print(df_results)
-                    except UserConfigValidationException as error:
-                        if "No counterfactuals found" in str(error):
-                            print(
-                                f"DICE could not find Counterfactuals for {query_instance}"
-                            )
-                    except IndexError as error:
+                    else:
                         print(
-                            f"fico failure when num_required_cfs={num_required_cfs}"
+                            f"DICE could not find Counterfactuals"
+                            f" for {query_instance}"
                         )
-
-    #         # NOTE: For an unexplained reason, the input df expects the
-    #         # target column to be included. Documentation does not mention
-    #         # this.
-    #         # http://interpret.ml/DiCE/notebooks/DiCE_getting_started.html
-    #         # combined_train_df = pd.concat([X_train, y_train], axis=1)
-    #
-    #         if model_name == "RF":
-    #             # backend = "sklearn"
-    #             # model = load_scikit_model(
-    #             #     f"counterfactual_explainers/models/{model_name}_{dataset}.pkl"
-    #             # )
-    #             # method = "random"
-    #             model = RandomForestClassifier()
-    #             model = model.fit(X_train, y_train)
-    #             m = Model(model=model, backend="sklearn")
-    #             exp = Dice(dice_data_object, m, method="random")
-    #             print(X_test[1:2])
-    #             e1 = exp.generate_counterfactuals(
-    #                 X_test[1:2], total_CFs=2, desired_class="opposite"
-    #             )
-    #             e1.visualize_as_list(show_only_changes=False)
-    #
-    #         elif model_name == "DNN":
-    #             # Use NN methods for DICE
-    #             backend = "TF2"
-    #             model = load_keras_model(
-    #                 f"counterfactual_explainers/models/{model_name}_{dataset}.keras"
-    #             )
-    #             method = "gradient"
-    #         else:
-    #             raise ValueError(
-    #                 f"Model configuration for '{model_name}' not found."
-    #             )
-    #         # Generate CF's print them for now
-    #         print(model)
-    #         predictions = model.predict(X_test_enc)
-    #         dice_model_object = Model(model=model, backend=backend)
-    #         counterfactual_explainer = Dice(
-    #             dice_data_object, dice_model_object, method=method
-    #         )
-    #
-    #         # Select a query instance
-    #         query_instance = X_test_enc[0:1]
-    #
-    #         # X_test_enc_df = pd.DataFrame(
-    #         #     query_instance, columns=features.columns
-    #         # )
-    #         # print(X_test_enc_df)
-    #         explanation = counterfactual_explainer.generate_counterfactuals(
-    #             query_instance, total_CFs=4, desired_class="opposite"
-    #         )
-    #
-    #         # Visualize the counterfactuals
-    #         explanation.visualize_as_dataframe(show_only_changes=True)
-    #
-    #         # Measure metrics
-    #
-    # # csv_path = package / "results" / "training.csv"
-    # # df_results = pd.DataFrame(results)
-    # # df_results.to_csv(csv_path, index=False)
-    # # print(df_results)
-    #
-    #
+                except UserConfigValidationException as error:
+                    if "No counterfactuals found" in str(error):
+                        print(
+                            "DICE could not find Counterfactuals"
+                            f" for {query_instance}"
+                        )
+                except IndexError:
+                    print(
+                        "fico failure when num_required_cfs="
+                        f"{num_required_cfs}"
+                    )
 
 
 if __name__ == "__main__":
